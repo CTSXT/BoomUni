445d000bc4fdcdb35f9e421fd6fe1b9e4fa542ad
David Haralambiev <david@davidharalambiev.tech>
2022-12-16 16:36:43 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,13 @@
 import "../scss/app.scss";
-const R = require("ramda");
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
   const ul = document.querySelector("ul");
   const numbersArray = [1, 3, 4, 5, 6, 7];
-  const isEven = (n) => n % 2 === 0;
-  const filteredArray = R.filter(isEven, numbersArray);
+   const filteredArray = numbersArray.filter(elem=> elem%2===0);
+filteredArray.forEach(elem=>{
+  const li = document.createElement('li');
+  li.innerText=elem;
+  ul.appendChild(li);
+});
 });
